860f509dfa0e012752908767443c653cddead46f
xjz <858959396@qq.com>
2019-12-01 16:52:59 +0800

    添加test18

---
+++ src/com/bjpowernode/jdbc/JDBCTest18.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.jdbc;
+
+/**
+ * className:JDBCTest18
+ * package:com.bjpowernode.jdbc
+ * Description:简单描述
+ *
+ * @data:2019/12/1 0001 16:52
+ * @author:xx
+ */
+public class JDBCTest18 {
+    public static void main(String[] args) {
+        System.out.println("wo tianj l ");
+    }
+}
